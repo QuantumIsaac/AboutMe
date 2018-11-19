0decd97c5bad8d044b3ed1ce7ffa3741c04eedba
QuantumIsaac <quantumisaac@gmail.com>
2018-11-19 15:17:26 -0600

    second commit, added pausing capabilities and a progress bar

--- audiomanager.js
+++ audiomanager.js
@@ -3,13 +3,34 @@ var clips = {};
 function load_audio(name, src){
 	var a = document.createElement('audio');
 	a.src = src;
+	a.addEventListener('timeupdate', update_time);
 	clips[name] = a;
 }
+
+//Controls
+var title = null;
+var toggler = null;
+var time_bar = null;
+
+function InitControls(){
+	title = document.getElementById("now-playing");
+	toggler = document.getElementById("toggler");
+	time_bar = document.getElementById("time-bar");
+}
+
+window.addEventListener('load', InitControls);
 
 //Player
 var playing = null;
 var in_fader = null;
 var out_fader = null;
+var end_time = -1;
+var start_time = -1;
+
+function update_time(){
+	console.log("time is " + playing.currentTime);
+	time_bar.value = parseInt(((playing.currentTime - start_time) / ((end_time+5) - start_time)) * 100);
+}
 
 function fade_in(){
 	if( playing == null ) return;
@@ -35,6 +56,7 @@ function play(name, start, end){
 	var a = clips[name];
 	if( a != null ){
 		if( playing != null ){
+			if( out_fader != null ) clearTimeout(out_fader);
 			stop();
 		}
 		if( in_fader != null ){
@@ -45,12 +67,27 @@ function play(name, start, end){
 		a.volume = 0;
 		a.play();
 		playing = a;
+		toggler.innerText = "Pause";
+		title.innerText = name;
+		start_time = start;
+		end_time = end;
 		fade_in();
-		setTimeout(fade_out, (end-start)*1000);
+		out_fader = setTimeout(fade_out, (end-start)*1000);
 	}
 }
 
 function stop(){
 	playing.pause();
 	playing = null;
+}
+function toggle(){
+	if( !playing.paused ){
+		clearTimeout(out_fader);
+		playing.pause();
+		toggler.innerText = "Play";
+	}else{
+		setTimeout(fade_out, parseInt(end_time - playing.currentTime)*1000);
+		playing.play();
+		toggler.innerText = "Pause";
+	}
 }
